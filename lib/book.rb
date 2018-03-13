8b1a1e42e98ad39084db6255af5993557af3cf4f
Maverick Tesini <mavericktesini@gmail.com>
2018-03-13 18:10:10 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,8 @@
 class Book
   attr_accessor :author, :page_count
-  attr_reader :title, :genre
-  GENRES = []
-  
+  attr_reader :title, :genre #only need to read, since manual write of CONSTANT is made
+  GENRES = []   #initalize an array for CONSTANT variable
+
   def initialize(title)
     @title = title
   end
@@ -11,7 +11,7 @@ class Book
     puts "Flipping the page...wow, you read fast!"
   end
 
-  def genre=(genre)
+  def genre=(genre)  #manual write/setter for CONSTANT, and shovel each new genre in CONSTANT array
     @genre = genre
     GENRES << genre
   end
