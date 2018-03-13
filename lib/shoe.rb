76c531cf4056ac04d613a2c580bf2ba1bd8ef60c
Maverick Tesini <mavericktesini@gmail.com>
2018-03-13 18:16:18 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,7 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  BRANDS = []
+  BRANDS = [] #CONSTANT given an empty array, to be filled.
 
   def initialize(brand)
     @brand = brand
